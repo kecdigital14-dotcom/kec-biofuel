b6027efb78ee9ea6027ef4a80cd9699015e8c262
kecdigital14-dotcom <kecdigital14@gmail.com>
2025-09-15 15:37:13 +0530

    Update Gallery.jsx

--- src/screens/Gallery.jsx
+++ src/screens/Gallery.jsx
@@ -42,8 +42,8 @@ const Home = () => {
 
        <GalleryScrollContent
         data={reaData}
-        sectionTitle="OUTREACH"
-        sectionSubtitle="PROGRAMS"
+        sectionTitle="AWARDS"
+        sectionSubtitle="RECOGNITION"
         sectionIcon="🌱"
       />
 
