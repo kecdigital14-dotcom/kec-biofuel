0b2951a5f57b3b9e2d767cfdc905862ea88f10ec
Priyanshu Gupta <gpriyanshu995@gmail.com>
2026-07-18 00:18:18 +0530

    fixed Navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,22 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          'Helvetica',
+          'Arial',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ],
+      },
+    },
   },
   plugins: [],
 }
